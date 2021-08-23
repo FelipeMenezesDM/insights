95421274372a05469e841670d7668f5fae02377e
Felipe Menezes <felipemenezes.web@gmail.com>
2021-08-22 22:48:56 -0300

    Separação de cabeçalho da tela inicial em componente que pode ser reutilizado.

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-toolbar
+    height="254"
+    dark
+    src="../assets/background.svg"
+    class="align-start home-header"
+    extended
+  >
+    <v-row class="d-flex flex-column ma-0">
+      <v-row class="d-flex justify-space-between ma-0" align="center">
+        <v-img src="../assets/brand-insights@3x.svg" max-width="50" max-height="32"></v-img>
+        <v-avatar width="56" height="56" class="h-avatar">
+          <v-img src="../assets/logo.png" alt="Felipe Menezes"></v-img>
+        </v-avatar>
+        <v-sheet width="50" color="transparent" class="text-end">
+          <v-btn small icon @click="addInsight()">
+            <v-icon color="#ED4D77">
+              mdi-plus
+            </v-icon>
+          </v-btn>
+        </v-sheet>
+      </v-row>
+      <v-row align="center" class="d-flex flex-column mt-3 home-page-title ma-0">
+        <h3 class="font-italic font-weight-light">Olá, Usuário!</h3>
+        <h5 class="font-italic">usuario@example.com</h5>
+      </v-row>
+      <v-row align="center" class="d-flex flex-column py-4 sep-bar ma-0">
+        <v-sheet color="#ED4D77" width="24" height="2"></v-sheet>
+      </v-row>
+      <v-row align="center" class="d-flex flex-column ma-0 feed-label">
+        <h3 class="font-weight-light font-italic">Feed de <span class="font-weight-medium">Insights</span></h3>
+      </v-row>
+    </v-row>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  methods: {
+    addInsight: function() {
+      this.$router.push('/add-insight');
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,6 @@
 <template>
   <div class="home-page">
-    <v-toolbar
-      height="254"
-      dark
-      src="../assets/background.svg"
-      class="align-start home-header"
-      extended
-    >
-      <v-row class="d-flex flex-column ma-0">
-        <v-row class="d-flex justify-space-between ma-0" align="center">
-          <v-img src="../assets/brand-insights@3x.svg" max-width="50" max-height="32"></v-img>
-          <v-avatar width="56" height="56" class="h-avatar">
-            <v-img src="../assets/logo.png" alt="Felipe Menezes"></v-img>
-          </v-avatar>
-          <v-sheet width="50" color="transparent" class="text-end">
-            <v-btn small icon @click="addInsight()">
-              <v-icon color="#ED4D77">
-                mdi-plus
-              </v-icon>
-            </v-btn>
-          </v-sheet>
-        </v-row>
-        <v-row align="center" class="d-flex flex-column mt-3 home-page-title ma-0">
-          <h3 class="font-italic font-weight-light">Olá, Usuário!</h3>
-          <h5 class="font-italic">usuario@example.com</h5>
-        </v-row>
-        <v-row align="center" class="d-flex flex-column py-4 sep-bar ma-0">
-          <v-sheet color="#ED4D77" width="24" height="2"></v-sheet>
-        </v-row>
-        <v-row align="center" class="d-flex flex-column ma-0 feed-label">
-          <h3 class="font-weight-light font-italic">Feed de <span class="font-weight-medium">Insights</span></h3>
-        </v-row>
-      </v-row>
-    </v-toolbar>
+    <HomeHeader />
     <v-container style="margin-top: -95px" class="pa-4">
       <v-row class="ma-0">
         <v-col cols="12" class="pa-0">
@@ -70,6 +38,7 @@
 </template>
 
 <script>
+import HomeHeader from '../components/HomeHeader.vue';
 import Card from '../components/Card.vue';
 import Insight from '../services/insight';
 
@@ -81,12 +50,8 @@ export default {
     }
   },
   components: {
-    Card
-  },
-  methods: {
-    addInsight: function() {
-      this.$router.push('/add-insight');
-    }
+    Card,
+    HomeHeader
   },
   mounted() {
     Insight.list().then(result => {
